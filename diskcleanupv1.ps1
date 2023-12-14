09e415ed5647e970a495d36bf5d427374109782d
dubberness <56581444+dubberness@users.noreply.github.com>
2023-12-14 19:59:55 +1100

    redoing profile scanning, deleted profile deletion

--- diskcleanupv1.ps1
+++ diskcleanupv1.ps1
@@ -14,11 +14,22 @@ $WinInstallDate = (Get-CimInstance Win32_OperatingSystem).InstallDate
 $CurrentDate = Get-Date
 
 # Start of profile analysis
-Write-Host "Found the following Profiles:"
+Write-Host "Scanning for Profiles..."
 
-# Iterate over each profile
+# List all non-system profiles
+Write-Host "Non-System Profiles Found:"
 $ComputerProfiles | ForEach-Object {
-    # Retrieve profile information
+    $profileInfo = Get-ItemProperty "$ProfilePath\$($_.PSChildName)"
+    $ProfileName = [System.IO.Path]::GetFileName($profileInfo.ProfileImagePath)
+
+    if(-not ($ProfileName -eq 'SystemProfile' -or $ProfileName -eq 'LocalService' -or $ProfileName -eq 'NetworkService' -or $ProfileName -like '*Service')){
+        Write-Host "  Profile: $ProfileName"
+    }
+}
+
+# Separately list profiles that will be deleted
+Write-Host "`nProfiles Planned for Deletion:"
+$ComputerProfiles | ForEach-Object {
     $profileInfo = Get-ItemProperty "$ProfilePath\$($_.PSChildName)"
     $ProfileName = [System.IO.Path]::GetFileName($profileInfo.ProfileImagePath)
 
@@ -35,95 +46,13 @@ $ComputerProfiles | ForEach-Object {
         $LastLogOff = ([System.DateTimeOffset]::FromFileTime($NTLogoffEpoch)).DateTime
     }
 
-    # Check if $LastLogOff is a valid date and calculate logoff age
-    $LogoffAgeDays = $null
+    # Calculate logoff age and list profiles for deletion
     if($LastLogOff -and $LastLogOff -ne [datetime]::MinValue){
         $LogoffAgeDays = ($CurrentDate - $LastLogOff).Days
-    }
 
-    # Skip profiles with a logoff age of 0 days
-    if($LogoffAgeDays -eq 0){
-        return
-    }
-
-    # Output profile details if above conditions are not met
-    if($LastLogOff){
-        Write-Host "Profile: $($profileInfo.ProfileImagePath)"
-        Write-Host "Last Logoff Age: $($LogoffAgeDays) days"
-    }
-}
-
-Write-Host "`n Deleting profiles:"
-
-foreach($Profile in $ComputerProfiles){
-    $NTLogonEpoch = $null
-    $LastLogOn = $null
-    $NTLogoffEpoch = $null
-    $LastLogOff = $null
-    $Delete = $null
-    $Keep = $false
-    $ProfileValues = Get-ItemProperty "$ProfilePath\$($Profile.PSChildName)"
-
-    $ProfileName = [System.IO.Path]::GetFileName($ProfileValues.ProfileImagePath)
-    if($ProfileName -eq 'SystemProfile' -or $ProfileName -eq 'LocalService' -or $ProfileName -eq 'NetworkService'){
-        Write-Host "Skipping system profile: $ProfileName"
-        continue
-    }
-    if(($ProfileValues.LocalProfileLoadTimeHigh) -and ($ProfileValues.LocalProfileLoadTimeLow)){
-        [long]$NTLogonEpoch = "0x{0:X}{1:X}" -f $ProfileValues.LocalProfileLoadTimeHigh, $ProfileValues.LocalProfileLoadTimeLow
-        $LastLogOn = ([System.DateTimeOffset]::FromFileTime($NTLogonEpoch)).DateTime
-        
-        if(($LastLogOn -lt $AgeDate) -and ($LastLogOn -gt $AgeMaxThreshold)){
-            $Delete = "Logon Date"
-        } else{
-            $Keep = $true
-        }
-    }
-    if(($ProfileValues.LocalProfileUnloadTimeHigh) -and ($ProfileValues.LocalProfileUnloadTimeLow)){
-        [long]$NTLogoffEpoch = "0x{0:X}{1:X}" -f $ProfileValues.LocalProfileUnloadTimeHigh, $ProfileValues.LocalProfileUnloadTimeLow
-        $LastLogOff = ([System.DateTimeOffset]::FromFileTime($NTLogoffEpoch)).DateTime
-        
-        if(($LastLogOff -lt $AgeDate) -and ($LastLogOff -gt $AgeMaxThreshold)){
-            $Delete = "Logoff Date"
-        } else{
-            $Keep = $true
-        }
-    }
-    try{
-        # Get the user account name from SID
-        $objSID = New-Object System.Security.Principal.SecurityIdentifier("$($Profile.PSChildName)")
-        $UserID = $objSID.Translate([System.Security.Principal.NTAccount])
-    } catch [System.Management.Automation.MethodInvocationException]{
-        Write-Host -Entry "$($Profile.PSChildName) does not exist for profile $($ProfileValues.ProfileImagePath)" -EntryType 2
-        $UserID = 'Unknown'
-    }
-    if(!$Delete -and !$Keep -and ($WinInstallDate -lt $AgeDate)){
-        # Profile is probably a Run As, delete it.
-        $Delete = "Run As Profile"
-    }
-    if(($Delete) -and ($UserID -notin $LoggedOnUsers)){
-        # Delete the profile, capture all output streams and log it.
-        $DeleteResults = (Get-CimInstance -Class Win32_UserProfile | Where-Object{ $_.SID -eq "$($Profile.PSChildName)"} | Remove-CimInstance -ErrorAction SilentlyContinue -Verbose) *>&1
-        if($?){
-            $Removed = $true
-        } else{
-            $Removed = $false
-        }
-        $Output = @"
-UserID: $UserID
-UserSID: $($Profile.PSChildName)
-ProfileType: [$Delete]
-Guid: $($ProfileValues.Guid)
-LastLogon: $LastLogOn
-LastLogoff: $LastLogOff
-Output: $DeleteResults
-ProfileImagePath: $($ProfileValues.ProfileImagePath)
-"@
-        if(!$Removed){
-            Write-Log -Entry "$Output" -EntryType 3
-        } else{
-            Write-Log -Entry "$Output" -EntryType 1
-            Write-Host "Successfully removed profile for $UserID"
+        # List profile if logoff age is more than 0 days
+        if($LogoffAgeDays -gt 0){
+            Write-Host "  Profile: $ProfileName (Last Logoff: $LastLogOff)"
         }
     }
 }
