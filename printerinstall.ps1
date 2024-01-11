a7d10657fe7da4b34b5f8329f191c12a45db2cad
dubberness <56581444+dubberness@users.noreply.github.com>
2024-01-12 07:31:33 +1100

    bugfix

--- printerinstall.ps1
+++ printerinstall.ps1
@@ -2,5 +2,5 @@ Expand-Archive z99286L1b.zip -DestinationPath C:\temp\printer
 pnputil /add-driver "C:\temp\printer\z99286L1b\disk1\oemsetup.inf"
 Add-PrinterDriver -Name "PCL6 Driver for Universal Print"
 Add-PrinterPort -Name "Downstairs Printer Port" -PrinterHostAddress 10.0.0.151
-Add-Printer -DriverName "PCL6 Driver for Universal Print" -Name "Downstairs Printer" -PortName "DownstairsPrinterPort"
+Add-Printer -DriverName "PCL6 Driver for Universal Print" -Name "Downstairs Printer" -PortName "Downstairs Printer Port"
 Set-PrintConfiguration  -PrinterName "Downstairs Printer" -Color $false -PaperSize A4
